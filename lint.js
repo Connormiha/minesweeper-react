1a6dfb7001dc7354ea0207fd2497b17e63ceb587
Mihail Bodrov <connormiha1@gmail.com>
2018-06-15 02:21:40 +0300

    React 16.4.1 :arrow_up:

--- lint.js
+++ lint.js
@@ -6,10 +6,10 @@ const PRE_COMMIT = process.env.NODE_ENV === 'pre_commit';
 const EXEC_OPTIONS = {stdio: [0, 1, 2]};
 
 const runEslint = (files = 'src __tests__') =>
-    execSync(`./node_modules/.bin/eslint ${files} & echo "eslint success"`, EXEC_OPTIONS);
+    execSync(`./node_modules/.bin/eslint ${files} && echo "eslint success"`, EXEC_OPTIONS);
 
 const runStylint = (files = 'src') =>
-    execSync(`./node_modules/.bin/stylint ${files} & echo "stylint success"`, EXEC_OPTIONS);
+    execSync(`./node_modules/.bin/stylint ${files} && echo "stylint success"`, EXEC_OPTIONS);
 
 if (PRE_COMMIT) {
     let changedFiles = execSync('git diff --cached --name-only --diff-filter=ACM src __tests__', {encoding: 'utf8'});
