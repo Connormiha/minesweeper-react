7ccdadc2da287f8dfed89cfa37a513faebc8e280
Mihail Bodrov <connormiha1@gmail.com>
2018-07-20 03:17:26 +0300

    Empty gh-pages :tada:

--- __tests__/setup.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import React from 'react';
-
-global.React = React;
-
-process.env.ROOT_URL = '';
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import reducers from './reducers';
-import {createStore} from 'redux';
-import {enableBatching} from 'redux-batched-actions';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
-
-export const createAppStore = () =>
-    createStore(enableBatching(reducers), composeWithDevTools());
-
-export default createAppStore();
